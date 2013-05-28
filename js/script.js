02bc0f6b8471182c61720069c6d391993778baab
Mckenzie Murphy <mckenzie.murphy9@gmail.com>
2013-05-28 12:43:13 -0700

    finished js

--- js/script.js
+++ js/script.js
@@ -1,20 +1,99 @@
 $(document).ready(function(){
-		
-	$('#element').tooltip();
-	$('#element1').tooltip();
-	$('#element2').tooltip();
 	
-	$('.temp').click(function(){
-		$('#tempModal').modal(function(){
-			$(this).show();
+	$('.roll9').click(function(){
+		$('#roll9').modal(function(){
+			$(this).show()
 			});
 		});
 	
-	$('a').click(function(){
-		$('html, body').animate({
-			scrollTop: $( $.attr(this, 'href') ).offset().top
-		}, 500);
-		return false;
-	})
+	$('.roll8').click(function(){
+		$('#roll8').modal(function(){
+			$(this).show()
+			});
+		});
+	
+	$('.roll7').click(function(){
+		$('#roll7').modal(function(){
+			$(this).show()
+			});
+		});
+
+	$('.roll6').click(function(){
+		$('#roll6').modal(function(){
+			$(this).show()
+			});
+		});
+	
+	$('.roll5').click(function(){
+		$('#roll5').modal(function(){
+			$(this).show()
+			});
+		});
+	
+	$('.roll4').click(function(){
+		$('#roll4').modal(function(){
+			$(this).show()
+			});
+		});
+	
+	$('.roll3').click(function(){
+		$('#roll3').modal(function(){
+			$(this).show()
+			});
+		});
+
+	$('.roll2').click(function(){
+		$('#roll2').modal(function(){
+			$(this).show()
+			});
+		});
+	
+	$('.roll1').click(function(){
+		$('#roll1').modal(function(){
+			$(this).show()
+			});
+		});
+	/*descriptions*/	
+	$('#sprialRaw').tooltip();
+	$('#scallopRaw').tooltip();
+	$('#banzaiRaw').tooltip();
+	$('#spiral').tooltip();
+	$('#nori').tooltip();
+	$('#tunaSpicy').tooltip();
+	$('#tempura').tooltip();
+	$('#tempura2').tooltip();
+	$('#sheilaSpicy').tooltip();
+	$('#volcanoSpicy').tooltip();
+	$('#spiderRaw').tooltip();
+	$('#tempura3').tooltip();
+	$('#masago2').tooltip();
+	
+	/*Prices*/	
+	$('#bigTemp').tooltip();	
+	$('#tobiko').tooltip();
+	$('#banzai').tooltip();
+	$('#scallop').tooltip();
+	$('#tuna').tooltip();
+	$('#sheila').tooltip();
+	$('#volcano').tooltip();
+	$('#spider').tooltip();
+		
+	$('#all').click(function(){
+		$(".spicy").show()
+		$(".normal").show()
+		$(".raw").show()
+		});
+		
+	$('#spicy').click(function(){
+		$(".raw").fadeOut('slow')
+		$(".normal").fadeOut('slow')
+		$(".spicy").show()
+		});
+		
+	$('#raw').click(function(){
+		$(".spicy").fadeOut('slow'),
+		$(".normal").fadeOut('slow'),
+		$(".raw").show()
+		});
 	
 });
